160efee6514aff3242c49fb897088acba135d1f2
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2025-07-31 18:19:47 +0530

    version7.2

--- Basics/Part-3 (C++ STLs)/priority_queue.cpp
+++ Basics/Part-3 (C++ STLs)/priority_queue.cpp
@@ -10,10 +10,13 @@ int main()
     pq.push(2);
     pq.push(10);
     pq.push(8);
-
+    // time complexity of push is O(logn)
+    
     cout << pq.top() << endl;
-
+    // time complexity of top is O(1)
+    
     pq.pop();
+    // time complexity of pop is O(logn)
     cout << pq.top() << endl;
 
     // Minimum Heap:
