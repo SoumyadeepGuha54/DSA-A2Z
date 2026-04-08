cb9f28d400eb1c626a0a9dea18c286a1a09c778d
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-04-09 01:03:50 +0530

    version17.3

--- Basics/Part-4 (Basic Math)/armstrong_number.cpp
+++ Basics/Part-4 (Basic Math)/armstrong_number.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int power(int x, int e)
+{
+    int res = 1;
+    for (int exp = 0; exp < e; exp++) res *= x;
+    return res;
+}
+
 bool armstrong(int x)
 {
-    int n = 0, temp = x;
+    int n = 0, temp = x, cnt = log10(x) + 1;
     while (temp > 0)
     {
-        n += ((temp % 10) * (temp % 10) * (temp % 10));
+        n += power((temp % 10), cnt);
         temp /= 10;
     }
     return (x == n);
@@ -14,7 +21,7 @@ bool armstrong(int x)
 
 int main()
 {
-    int n1 = 153, n2 = 121, n3 = 10;
+    int n1 = 153, n2 = 121, n3 = 1634;
     cout << armstrong(n1) << " " << armstrong(n2) << " " << armstrong(n3);
     return 0;
 }
