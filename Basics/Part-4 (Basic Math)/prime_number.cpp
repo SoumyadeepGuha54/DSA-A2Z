4750c29615e98f8c04e4f7df5ffcb9989bcee875
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-04-09 00:39:19 +0530

    version17.2

--- Basics/Part-4 (Basic Math)/prime_number.cpp
+++ Basics/Part-4 (Basic Math)/prime_number.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool prime(int x)
+bool prime_v1(int x)
 {
     int cnt = 0;
     for (int i = 1; i <= x; i++)
@@ -12,9 +12,24 @@ bool prime(int x)
     else return false;
 }
 
+bool prime_v2(int x)
+{
+    if(x == 1) return false;
+    else
+    {
+        int cnt = 0;
+        for (int i = 1; i * i <= x; i++)
+        {
+            if (x % i == 0) cnt++;
+        }
+        if (cnt == 1) return true;
+        else return false;
+    }
+}
+
 int main()
 {
-    int n = 3;
-    cout << prime(n);
+    int n = 1;
+    cout << prime_v1(n) << " " << prime_v2(n);
     return 0;
 }
