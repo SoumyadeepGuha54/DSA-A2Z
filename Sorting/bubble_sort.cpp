0d4966c4ab63a59025b86a5eebcb92804dc7a204
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-04-18 00:50:59 +0530

    version29.2

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) {
     for(int i = 0; i < n - 1; i++) {
-        for(int j = i; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        int isSorted = 0;
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                isSorted = 1;
+            }
         }
+        if(isSorted == 0) break;
     }
 }
 
