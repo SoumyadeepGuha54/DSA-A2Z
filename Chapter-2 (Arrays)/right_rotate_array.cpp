9a69f9d154e272bedc71f1adbae52e349d9f0373
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-04-22 17:34:51 +0530

    version39

--- Chapter-2 (Arrays)/right_rotate_array.cpp
+++ Chapter-2 (Arrays)/right_rotate_array.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 // Brute Force Approach:
-void leftRotate1(vector<int> &nums, int k) {
+void rightRotate1(vector<int> &nums, int k) {
     vector<int> t;
     int n = nums.size();
-    for(int i = 0; i < k; i++) t.emplace_back(nums[i]);
-    for(int i = k; i < n; i++) nums[i - k] = nums[i];
-    for(int i = n - k; i < n; i++) nums[i] = t[i - n + k];
+    for(int i = n - k; i < n; i++) t.emplace_back(nums[i]);
+    for(int i = n - 1; i > k - 1; i--) nums[i] = nums[i - k];
+    for(int i = 0; i < k; i++) nums[i] = t[i];
 }
 // time complexity is O(n)
 
 // Optimal Solution:
-void leftRotate2(vector<int> &nums, int k) {
+void rightRotate2(vector<int> &nums, int k) {
     vector<int> t;
     k = k % nums.size();
+    reverse(nums.begin(), nums.end());
     reverse(nums.begin(), nums.begin() + k);
     reverse(nums.begin() + k, nums.end());
-    reverse(nums.begin(), nums.end());
 }
 // time complexity is O(n)
 
 int main() {
     vector<int> v = {12, 23, 34, 45, 56, 67, 78};
-    leftRotate1(v, 3);
+    rightRotate1(v, 3);
     for(auto it : v) cout << it << " ";
     cout << endl;
-    leftRotate2(v, 4);
+    rightRotate2(v, 4);
     for(auto it : v) cout << it << " ";
     return 0;
 }
